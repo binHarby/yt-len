bfaed4814791694a62209735804c3617beacd7c7
binHarby <lance.alameri@gmail.com>
2022-04-08 11:25:55 +0400

    shell scripts path changes, change license in PKGBUIL

--- Linux/install-linux.sh
+++ Linux/install-linux.sh
@@ -4,7 +4,7 @@ echo "Installing yt-len dependancies ===>"
 pip install -r ../requirements.txt --user
 
 echo "Installing yt-len ===>"
-sudo cp dist/yt-len /usr/bin/yt-len
+sudo cp ../yt-len /usr/bin/yt-len
 sudo chmod +x /usr/bin/yt-len
 
 echo "===>Done."
